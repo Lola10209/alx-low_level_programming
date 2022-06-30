22efdd929627d8c7fccd0300fceb9c50554a1097
lola10209 <shanteltestimony@gmail.com>
2022-06-30 11:25:54 -0700

    Current directory

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,15 +4,8 @@
  * main - this is the starting point of the program
  * Return: returns 0
  */
-
 int main(void)
-
 {
-
-		puts("Programming is like building a multilingual puzzle");
-
-			puts("\"Programming is like building a multilingual puzzle");
-
-				return (0);
-
+	puts("Programming is like building a multilingual puzzle");
+       	return (0);
 }

ddfa8c0eb327e891bcb5a72e74ec73f671b3aac6
lola10209 <shanteltestimony@gmail.com>
2022-07-01 07:25:04 -0700

    Current directory

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastDigit = n % 10;
+	lastDigit = n % 10; is assign to a random number to the variable n each time it is executed using another if statement
 	printf("Last digit of %d is ", n);
 	if (lastDigit > 5)
 		printf("%d and is greater than 5\n", lastDigit);

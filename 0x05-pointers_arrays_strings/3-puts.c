27b97667b047c1cf13a9d18a56e627800b51724d
lola10209 <shanteltestimony@gmail.com>
2022-07-12 06:27:11 -0700

    Current directory

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,14 +2,15 @@
 
 /**
  * _puts - prints a string
- * @str: the string to be printed
+ * @str: string to be printed
  *
+ * Return: void
  */
 void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	for (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;

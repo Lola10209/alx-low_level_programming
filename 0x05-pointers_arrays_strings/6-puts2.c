6a6ae4bcee04d57e228748e130d1d5147ba50509
lola10209 <shanteltestimony@gmail.com>
2022-07-12 08:56:16 -0700

    Current directory

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 -> puts2 function
+ * @str: str para8meter
+ * Return: No return
+ **/
+
+void puts2(char *str)
+
+{
+	int len, i;
+
+	len = strlen(str);
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}

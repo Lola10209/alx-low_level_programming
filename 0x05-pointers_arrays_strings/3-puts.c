30c7c3d22c4ace8bbff059aeff39d34a2e7a8546
lola10209 <shanteltestimony@gmail.com>
2022-07-12 07:38:21 -0700

    Current directory

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,11 +4,13 @@
  * _puts - prints a string
  * @str: string to be printed to _puts function
  */
-void _puts(char *str)
+void _puts(char *);
+
+int main(void)
 {
-	for (; *str != '\0'; str++)
-	{
-		_putchar(*str);
-	}
-	_putchar('\n');
+	char *str;
+
+	str = "ALX!";
+	_puts(str);
+	return (0);
 }

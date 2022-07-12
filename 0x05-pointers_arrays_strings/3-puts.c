4d4b82d9790619c0c522236c8e5a0858ed492f4e
lola10209 <shanteltestimony@gmail.com>
2022-07-12 06:14:50 -0700

    Current directory

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,18 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _puts - prints a string
- * @str: the string to print
+ * @str: the string to be printed
  *
- * Return: void
  */
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
+	int i = 0;
 
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
 	_putchar('\n');
 }

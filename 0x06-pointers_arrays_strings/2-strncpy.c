4614fb1f6bc7b1ba2e30b1084ba5be270fecbf5f
lola10209 <shanteltestimony@gmail.com>
2022-07-14 13:41:07 -0700

    Current directory

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy -> for copying purposes
+ * @dest: param 1
+ * @src: param 2
+ * @n: param 3
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}

66e1edd552174c81f7ceedf690e21cc0c51ad861
lola10209 <shanteltestimony@gmail.com>
2022-07-14 21:37:25 -0700

    Current directory

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,24 +6,18 @@
  * @a: first parameter
  * @n: second param no. of elements
  *
- * Return: empty
+ * Return: returns nothing
  */
 void reverse_array(int *a, int n)
 {
-	int *p, i, aux, k;
+	int i, tmp;
+	int ln = n - 1;
 
-	p = a;
-
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n / 2; i++)
 	{
-		p++;
-	}
-
-	for (k = 0; k < i / 2; k++)
-	{
-		aux = a[k];
-		a[k] = *p;
-		*p = aux;
-		p--;
+		tmp = a[i];
+		a[i] = a[ln];
+		a[ln] = tmp;
+		ln--;
 	}
 }

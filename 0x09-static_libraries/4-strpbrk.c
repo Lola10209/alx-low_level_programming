36b439d923fa7aeda3fccf641150e990028e260b
lola10209 <shanteltestimony@gmail.com>
2022-07-23 13:04:18 -0700

    Current directory

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,24 +1,15 @@
+#include <string.h>
 #include "main.h"
-#include <stddef.h>
 
 /**
- * _strbrk - a function that searches a string for any of a set of bytes.
- * @s: string to be searched
- * @accept: string to be searched for
+ * _strpbrk - search a string for any of a set of bytes
+ * @s: pointer to the main string
+ * @accept: pointer to the substring
  *
- * Return: The pointer to the first occurence
+ * Return: the number of bytes in the initial segment of s
  */
+
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; accept[j] != '\0'; j++)
-		{
-			if (s[i] == accept[j])
-				return (&s[i]);
-		}
-	}
-	return (NULL);
+	return (strpbrk(s, accept));
 }

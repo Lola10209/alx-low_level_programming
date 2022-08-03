896ca2bdd91f3a8000cb982db78dcf0de0ec8fc1
lola10209 <shanteltestimony@gmail.com>
2022-08-03 08:36:58 -0700

    Current directory

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 /**
  * print_name - print a name using function call back
  * @name: pointer to the name to be printed
- * @r: function pointer to call back a function
+ * @f: function pointer to call back a function
  */
 
 void print_name(char *name, void (*f)(char *))

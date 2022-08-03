6904777cc828b318e0747f32c487e0748075782b
lola10209 <shanteltestimony@gmail.com>
2022-08-03 07:30:49 -0700

    Current directory

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,8 @@
 /**
  * print_name - a function that prints a name
  * @name: the name to be printed
- * @f: a pointer to a function that accepts a string as parameter and returns nothing
+ * @f: a pointer to a function that accepts a string as
+ * parameter and returns nothing
  * Return: returns nothing
  */
 void print_name(char *name, void (*f)(char *))

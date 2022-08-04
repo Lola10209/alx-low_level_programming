16eb9fbe1c140159047970fe6dde6eee7a2de53c
lola10209 <shanteltestimony@gmail.com>
2022-08-03 19:20:31 -0700

    Current directory

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,16 +1,19 @@
-#include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
 
 /**
- * main - ...
- * @argc: ...
- * @argv: ...
- * Return: ...
+ * main - a program that performs simple arithmetic operations by calling
+ * helper functions
+ * @argc: the number of command-line arguments
+ * @argv: an array of strings containing one command-line argument per string
+ * Return: returns 0 (success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char *[])
 {
-	int (*oprt)(int, int);
+	int num1, num2, result;
+	int (*ptr)(int, int);
 
 	if (argc != 4)
 	{
@@ -18,14 +21,25 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	oprt = get_op_func(argv[2]);
-
-	if (!oprt)
+	if (strlen(argv[2]) == 1 && (argv[2][0] == '+' || argv[2][0] == '-'
+		|| argv[2][0] == '*' || argv[2][0] == '/' || argv[2][0] == '%%'))
 	{
-		printf("Error\n");
-		exit(99);
-	}
+		if (atoi(argv[3]) == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
+		{
+			printf("Error\n");
+			exit(100);
+		}
 
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
-	return (0);
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[3]);
+
+		ptr = get_op_func(argv[2]);
+		result = ptr(num1, num2);
+
+		printf("%d\n", result);
+
+		return (0);
+	}
+	printf("Error\n");
+	exit(99);
 }

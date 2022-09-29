772a70c60cc182b478f90bdefc2d184c0d9142f0
lola10209 <shanteltestimony@gmail.com>
2022-09-29 16:19:59 -0700

    FIXED

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,12 +2,12 @@
 
 /**
  * dlistint_len - return the number of elements in a linked dlistint_t list.
- * @n: pointer to the list.
+ * @h: pointer to the list.
  * Return: number of nodes.
  */
-size_t dlistint_len(const dlistint_t *n)
+size_t dlistint_len(const dlistint_t *h)
 {
-	const dlistint_t *node = n;
+	const dlistint_t *node = h;
 	size_t cont = 0;
 
 	while (node)
